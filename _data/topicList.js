2f4c414a3d14d96b7d4f4ecae33b3e9cf3d6670c
E-ZPark <68869058+E-ZPark@users.noreply.github.com>
2026-07-21 17:54:25 +0300

    Add topicList.js with predefined topics

---
+++ _data/topicList.js
@@ -0,0 +1,9 @@
+module.exports = [
+  { name: "Chumash", slug: "chumash" },
+  { name: "Tefillah", slug: "tefillah" },
+  { name: "Chagim", slug: "chagim" },
+  { name: "Halacha", slug: "halacha" },
+  { name: "Hashkafah/Machshavah", slug: "hashkafah-machshavah" },
+  { name: "Gemara/Talmud", slug: "gemara-talmud" },
+  { name: "Miscellaneous", slug: "miscellaneous" }
+];
